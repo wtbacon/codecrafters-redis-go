5dd0b674b3f0c7b3953db562e921660d0300ad25
wtbacon <wtbaaaacon@gmail.com>
2025-01-13 20:02:38 +0900

    feat: Respond to PING naively

--- app/server.go
+++ app/server.go
@@ -14,16 +14,28 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+	}
+
+	if string(buf[:n]) == "*1\r\n$4\r\nPING\r\n" {
+		conn.Write([]byte("+PONG\r\n"))
+	} else {
+		conn.Write([]byte("-ERR unknown command\r\n"))
 	}
 }
